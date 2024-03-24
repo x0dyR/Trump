b0bceba4b8bc5da44f9b84aa9a9b2d9863cd9570
2red1s <redartik13@gmail.com>
2024-03-24 15:13:42 +0600

    .

--- Assets/Scripts/Enemy/Troll.cs
+++ Assets/Scripts/Enemy/Troll.cs
@@ -1,67 +1,67 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// namespace collegeGame
-// {
-//     public class Troll : MonoBehaviour
-//     {
-//         public GameObject Player;
-//         private UnityEngine.AI.NavMeshAgent agent;
-//         private Animator animator; // Добавляем переменную аниматора
-
-//         private void Start()
-//         {
-//             agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-            
-//             // Получаем доступ к компоненту аниматора дочернего объекта с именем "Troll"
-//             animator = GetComponentInChildren<Animator>();
-//         }
-
-//         private void Update()
-//         {
-//             agent.destination = Player.transform.position;
-
-//             // Устанавливаем параметр анимации run, если скорость NavMeshAgent больше нуля
-//             if (agent.velocity.magnitude > 0)
-//             {
-//                 animator.SetBool("IsRunning", true);
-//             }
-//             else
-//             {
-//                 animator.SetBool("IsRunning", false);
-//             }
-//         }
-//     }
-// }
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class Troll : MonoBehaviour
+namespace collegeGame
 {
-    private NavMeshAgent agent;
-    private Animator animator;
-
-    private void Start()
+    public class Troll : MonoBehaviour
     {
-        agent = GetComponent<NavMeshAgent>();
-        animator = GetComponentInChildren<Animator>();
-    }
+        public GameObject Player;
+        private UnityEngine.AI.NavMeshAgent agent;
+        private Animator animator; // Добавляем переменную аниматора
 
-    private void Update()
-    {
-        if (!agent.hasPath || agent.remainingDistance < 0.5f)
+        private void Start()
         {
-            // Выбираем случайную точку внутри навигационной сетки
-            Vector3 randomPoint = Random.insideUnitSphere * 50f;
-            NavMeshHit hit;
-            NavMesh.SamplePosition(transform.position + randomPoint, out hit, 50f, NavMesh.AllAreas);
-
-            // Устанавливаем случайную точку как новую цель для NavMeshAgent
-            agent.SetDestination(hit.position);
+            agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
             
-            // Устанавливаем параметр анимации IsWalking
-            animator.SetBool("IsWalking", true);
+            // Получаем доступ к компоненту аниматора дочернего объекта с именем "Troll"
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        private void Update()
+        {
+            agent.destination = Player.transform.position;
+
+            // Устанавливаем параметр анимации run, если скорость NavMeshAgent больше нуля
+            if (agent.velocity.magnitude > 0)
+            {
+                animator.SetBool("IsRunning", true);
+            }
+            else
+            {
+                animator.SetBool("IsRunning", false);
+            }
         }
     }
 }
+// using UnityEngine;
+// using UnityEngine.AI;
+
+// public class Troll : MonoBehaviour
+// {
+//     private NavMeshAgent agent;
+//     private Animator animator;
+
+//     private void Start()
+//     {
+//         agent = GetComponent<NavMeshAgent>();
+//         animator = GetComponentInChildren<Animator>();
+//     }
+
+//     private void Update()
+//     {
+//         if (!agent.hasPath || agent.remainingDistance < 0.5f)
+//         {
+//             // Выбираем случайную точку внутри навигационной сетки
+//             Vector3 randomPoint = Random.insideUnitSphere * 50f;
+//             NavMeshHit hit;
+//             NavMesh.SamplePosition(transform.position + randomPoint, out hit, 50f, NavMesh.AllAreas);
+
+//             // Устанавливаем случайную точку как новую цель для NavMeshAgent
+//             agent.SetDestination(hit.position);
+            
+//             // Устанавливаем параметр анимации IsWalking
+//             animator.SetBool("IsWalking", true);
+//         }
+//     }
+// }
